7f3c447f7d79f46f65a63c1d3c2447a6ce5d4550
bibhutipanda <1705030@kiit.ac.in>
2020-07-27 16:08:08 +0530

    UI Swipe Tabs Implemented

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),100);
+        viewPagerAdapter.addFragment(new BlankFragment(),"Music");
+        viewPagerAdapter.addFragment(new BlankFragment(),"Movies");
+        viewPagerAdapter.addFragment(new BlankFragment(),"Podcasts");
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
